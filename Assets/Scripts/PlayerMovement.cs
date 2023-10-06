395e589bd6aa6bafe47a5644d5ac3b4f8445cc70
EduardoDarkP8 <edudupamplona@gmail.com>
2023-10-06 00:45:10 -0300

    11

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,17 +5,27 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     Rigidbody2D rg;
+    SpriteRenderer sr;
     public float velocity = 2f;
     float x, y;
     void Start()
     {
         rg = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
         x = Input.GetAxisRaw("Horizontal");
+		if (x > 0) 
+        {
+            sr.flipX = false;
+        }
+        else if (x < 0) 
+        {
+            sr.flipX = true;
+        }
     }
     private void FixedUpdate()
     {
